75c2dea174e70bd01ef5818276d467bc213ed397
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2025-09-02 20:14:04 +0000

    fix api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -15,7 +15,7 @@ const headersWithAuthorizeFn = () => ({
 });
 
 export const registerUser = (userData) => {
-  return fetch(`${URL}/signup/`, {
+  return fetch(`${URL}/users/signup`, {
     method: "POST",
     headers: headersWithContentType,
     body: JSON.stringify(userData),
@@ -23,7 +23,7 @@ export const registerUser = (userData) => {
 };
 
 export const loginUser = (username, password) => {
-  return fetch(`${URL}/signin/`, {
+  return fetch(`${URL}/signin`, {
     method: "POST",
     headers: headersWithContentType,
     body: JSON.stringify({ username, password }),
